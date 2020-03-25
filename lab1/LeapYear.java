8dbdd876bb3dea5580261ed2033f8e782ab0eaf1
DM <gsavdm@gmail.com>
2020-03-25 18:52:11 +0530

    lab1 submission

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,14 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author DM
  */
 public class LeapYear {
+
+    /** Checks if a year is leap year
+     *  @param year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        return ((year % 400) == 0 || (((year % 4) == 0) && ((year % 100) != 0)));
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
